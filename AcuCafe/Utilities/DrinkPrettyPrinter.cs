56d3aaa7c5972adbb979441ddc2f7c08625085d3
Ali Faizan Kazmi <alifaizankazmi@gmail.com>
2019-12-25 16:40:57 +0000

    Add null-safety

--- AcuCafe/Utilities/DrinkPrettyPrinter.cs
+++ AcuCafe/Utilities/DrinkPrettyPrinter.cs
@@ -6,6 +6,11 @@ namespace AcuCafe
   {
     public static string PrettyPrint(IDrink drink)
     {
+      if(drink == null)
+      {
+        return string.Empty;
+      }
+
       var message = $"We are preparing the following drink for you: {drink.Description} with ";
       message += string.Join(", ", drink.Ingredients.Select(i => i.Description));
 
